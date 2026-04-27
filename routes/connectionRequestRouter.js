9cdb0abdeed7e2f37f64b83886d0eaedc8ca613b
riyaverma <techriya054@gmail.com>
2026-04-27 23:49:08 +0530

    add validations for connection request( handled corner cases for connectionRequest)

--- routes/connectionRequestRouter.js
+++ routes/connectionRequestRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authUser= require("../middleware/auth.middleware")
 const ConnectionRequest = require("../models/connectionRequestSchema");
+const User = require("../models/userSchema");
 
 const connectionRouter = express.Router();
 
@@ -9,6 +10,35 @@ connectionRouter.post("/send/:status/:receiverId",authUser, async(req,res) =>{
     try{
         const senderId = req.user._id;
         const {receiverId, status} = req.params;
+        //check if the receiverId is valid or not
+        const receiver = await User.findById(receiverId);
+        if(!receiver){
+            throw new Error("person you want to connect with is not found");
+        }
+            //check if the connection request is already sent or not
+            const existingConnection = await ConnectionRequest.findOne({
+
+                $or:[
+                    {senderId, receiverId},
+                    {senderId:receiverId, receiverId:senderId}
+                ]
+            });
+            if(existingConnection){
+                throw new Error("connection request already sent");
+            }
+
+
+        //check status it should be either interested or rejected
+        if(status !== "interested" && status !== "rejected"){
+            throw new Error("status should be either interested or rejected");
+        }
+
+
+
+        //senderId and receiverId should not be same
+        if(senderId.toString() === receiverId.toString()){
+            throw new Error("you cannot send connection request to yourself");
+        }
         
         //send the connection request
         const connection = new ConnectionRequest({
