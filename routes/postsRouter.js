3d571538a625e15d7f15f7e703ac31b2e462528f
riyaverma <techriya054@gmail.com>
2026-06-02 16:14:40 +0530

    add authentication middleware to toggle-like and posts retrieval routes, implement pagination for post retrieval

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -6,7 +6,7 @@ const Post = require('../models/postSchema');
 
 const postsRouter = express.Router();
 
-postsRouter.patch('/:postId/toggle-like/:userId', async(req, res)=>{
+postsRouter.patch('/:postId/toggle-like/:userId',authUser, async(req, res)=>{
     try{
         const {postId, userId} =  req.params;
         const post = await Post.findById(postId);
@@ -34,14 +34,25 @@ postsRouter.patch('/:postId/toggle-like/:userId', async(req, res)=>{
 
 
 
-postsRouter.get('/posts/:profileId',async(req, res)=>{
+postsRouter.get('/posts/:profileId',authUser,async(req, res)=>{
     try{
         const {profileId} = req.params;
-        const posts = (await Post.find({ profileId :profileId }).populate('profileId', 'firstName lastName profileUrl').sort({ createdAt: -1 }))
-        if(!posts){
-            return res.status(404).json({message: "posts not found"})
-        }
-        res.json(posts);
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skipPage =(page-1)* limit;
+        const totalPosts = await Post.countDocuments({ profileId: profileId });
+
+
+        const posts = (await Post.find({ profileId :profileId }).populate('profileId', 'firstName lastName profileUrl').sort({ createdAt: -1 }).skip(skipPage).limit(limit))
+        res.status(200).json({
+      success: true,
+      posts,
+      currentPage: page,
+      totalPages: Math.ceil(totalPosts / limit),
+      totalPosts,
+      hasNextPage: page < Math.ceil(totalPosts / limit),
+      hasPrevPage: page > 1,
+    });
 
     }catch(err){
         res.status(500).json({message: err.message, stack: err.stack});
